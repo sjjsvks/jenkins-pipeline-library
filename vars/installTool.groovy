35369283e13408091c97efe90d43bd8c95653b2c
JR.JeyaGanesh <sjjsvks@yahoo.co.in>
2021-07-24 14:34:50 +0530

    Shared Library Creation

--- vars/installTool.groovy
+++ vars/installTool.groovy
@@ -1,13 +1,14 @@
 def call(String pkgName) {
 	String version = System.getProperty("java.version");
-	print "Java Version : "+version
-	
+	print "Java Version : "+version	
 	if (pkgName=="jdk-11.0.12_linux-x64_bin") {
 		java_home="/home/cloud_user/Desktop/jdk-11.0.12"
 		withEnv(["JAVA_HOME=${java_home}/bin"]) {
 			version = System.getProperty("java.version");
 			print "Java Version : "+version
-			sh "JAVA_HOME : ${JAVA_HOME}"
+			steps{
+				sh "JAVA_HOME : ${JAVA_HOME}"
+			}
 		}
 	}else{
 		print "Out of Scope"
