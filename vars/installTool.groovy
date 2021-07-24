3a4c3dafab933c63ec799d5a978433e0b473b8a7
JR.JeyaGanesh <sjjsvks@yahoo.co.in>
2021-07-24 14:44:35 +0530

    Shared Library Creation

--- vars/installTool.groovy
+++ vars/installTool.groovy
@@ -4,7 +4,7 @@ def call(String pkgName) {
 	if (pkgName=="jdk-11.0.12_linux-x64_bin") {
 		java_home="/home/cloud_user/Desktop/jdk-11.0.12"
 		withEnv(["JAVA_HOME=${java_home}/bin"]) {
-			String javaHome = System.getenv("JAVA_HOME");
+			javaHome = System.getenv("JAVA_HOME");
 			print "JAVA_HOME : "+javaHome
 		}
 	}else{
